93ce3f84b650a4c82284cbff45bf6dc505238c02
Paulo Orquillo <paulo.orquillo@gmail.com>
2021-07-22 22:50:20 +1200

    Initial Commit

---
+++ SwiftUISimpleCharts/SwiftUISimpleChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISimpleChartsApp.swift
+//  SwiftUISimpleCharts
+//
+//  Created by Paulo Orquillo on 22/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISimpleChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
